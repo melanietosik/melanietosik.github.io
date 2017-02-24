38149bf5124745f793c5ba00d7b808d5b82e2414
Melanie Tosik <melanie@wayblazer.com>
2017-02-24 14:55:53 -0600

    transferring old site

---
+++ js/hero.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function($){
+    function fullscreen(){
+        jQuery('#hero').css({
+            width: jQuery(window).width(),
+            height: jQuery(window).height()
+        });
+    }
+    fullscreen();
+
+  jQuery(window).resize(function() {
+       fullscreen();         
+    });
+});
