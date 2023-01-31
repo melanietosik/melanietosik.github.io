28cbeea23005f03c5191a09e80b73bae82d40b84
Melanie Tosik <melanie.tosik@buzzfeed.com>
2023-01-30 22:29:05 -0500

    road work ahead

--- _next/static/oROAzatBuW91FOp51agjh/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/21dec9d4b076af735e28.css","static/chunks/pages/index-33fe62391ea6c644982d.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
